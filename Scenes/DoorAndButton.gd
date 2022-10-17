d0f46cfb5de470b6b5b18d5d0cfa589f3411f198
Roblox-HACKER301801 <misshi93@kingshigh.school.nz>
2022-10-18 09:53:54 +1300

    Fixed Doors (Final fix) + level design additions.

--- Scenes/DoorAndButton.gd
+++ Scenes/DoorAndButton.gd
@@ -1,21 +1,13 @@
 extends Node2D
 
-var door_open
-
 func _ready():
-	$AnimationPlayer.play("ButtonUp")
+	$AnimationPlayerButton.play("ButtonUp")
 	$AnimationPlayer.play("DoorClosed")
-	door_open = false
+	
 
-func open_door():
-	if not door_open:
-		$AnimationPlayer.play("DoorOpen")
-		door_open = true
+
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("Player"):
-		$AnimationPlayer.play("ButtonDown")
-		open_door()
-
-func _on_Area2D_body_exited(body):
-	$AnimationPlayer.play("ButtonUp")
+		$AnimationPlayerButton.play("ButtonDown")
+		$AnimationPlayer.play("DoorOpen") # Replace with function body.
